63bfd695e01f88afb282bc825f4ff4e03e639e01
root <root@localhost.localdomain>
2020-12-01 23:31:19 +0800

    fix some bug

--- BuildPHPDocker.sh
+++ BuildPHPDocker.sh
@@ -2,10 +2,13 @@
 
 cp .docker-compose.yml.bak docker-compose.yml
 
-read -p "Input PHP Version (default n):" phpv
+read -p "Input PHP Version (default php7.0):" phpv
 read -p "Open xdebug (default yes):" xdebug
-if [ "" = "$docker" ]; then
+if [ "" = "$xdebug" ]; then
 	xdebug="yes"
+fi
+if [ "" = "$phpv" ]; then
+        phpv="php7.0"
 fi
 sed -i "s/dockername/$phpv/g" docker-compose.yml
 echo "php_v=$phpv" > .env
